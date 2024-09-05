e8d62c465252ea7a734edcd58bd539b9f172bd5a
Kinjal <kinjaldas100@gmail.com>
2024-09-05 17:22:44 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://kinjxlcodes.github.io/sidlabs/',  // Replace with your repo name
+  base: '/',  // Use root path for Vercel deployment
 });
